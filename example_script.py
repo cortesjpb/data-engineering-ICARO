678b9bcb0cf008395c5279a622f309ab7b9b3c2d
Benjamin Cortes <cortesjpb@gmail.com>
2021-02-08 19:56:03 -0300

    Second try

--- example_script.py
+++ example_script.py
@@ -1,3 +1,4 @@
 print('This is the first script coded from VS Code, outside the container')
 print('A ver si esta linea se guarda')
 print('Y esta linea la escribo desde el contenedor')
+print('a ver si ahora funciona')
